3ae735d90637f94e8d3356ca64d0fab9bb29ef33
Kamil Jankowski <kamil.jankowski.x@gmail.com>
2023-04-06 16:35:10 +0200

    Package action (#3)
    * Initial packaging
    
    * Fix typo
    
    * Remove make version
    
    * Add package.yml
    
    * Update package.yml
    
    * Update build-system
    
    * Update setup.py
    
    * Update setup.py
    
    * Update setup.py
    
    * Disable fail-fast
    
    * Update setup.py
    
    * Remove linux
    
    * Fix repository_url
    
    * Ubuntu musllinux_x86_64
    
    * Update build.sh
    
    * *"linux"*
    
    * Windows
    
    * v0.1.1
    
    * Update setup.py
    
    * Update run_tests.yml

--- build.sh
+++ build.sh
@@ -1,4 +1,31 @@
-cd poseidon/sources
-make clean
-make only_c
-cp lib_pos.so ../../.
+#!/bin/bash
+
+build_for_mac_target () {
+    MACARCH=$(awk -F '-' '{print $1}' <<< $1)
+
+    pushd poseidon/sources
+    sed "/CFLAGS = -Wall -O3 -fPIC/ s/$/ -target $1/" Makefile > "Makefile_$MACARCH"
+    make -f "Makefile_$MACARCH" only_c
+    rm *.o "Makefile_$MACARCH"
+    popd
+
+    mv poseidon/sources/lib_pos.so "$(awk -F '-' '{print $1}' <<< $1)_lib_pos.dylib"
+}
+
+echo "Building poseidon..."
+
+if [[ "$OSTYPE" == *"linux"* ]]; then
+    make -C poseidon/sources only_c
+    mv poseidon/sources/lib_pos.so lib_pos.so
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    build_for_mac_target "x86_64-apple-macos11"
+    build_for_mac_target "arm64-apple-macos11"
+    lipo -create -output lib_pos.dylib x86_64_lib_pos.dylib arm64_lib_pos.dylib
+
+    find . -type f -not -name lib_pos.dylib -name '*.dylib' -delete
+else
+    echo "$OSTYPE is not supported at the moment"
+    exit 1;
+fi
+
+make -C poseidon/sources clean
---
+++ setup.py
@@ -0,0 +1,50 @@
+import subprocess
+import setuptools
+from setuptools.command.build_py import build_py
+from setuptools.command.build_ext import build_ext
+
+class PoseidonExtension(setuptools.Extension):
+    def __init__(self):
+        self.name = "poseidon"
+        setuptools.Extension.__init__(self, self.name, sources=[])
+
+
+class BuildPy(build_py):
+    def run(self):
+        self.run_command("build_ext")
+        return super().run()
+
+
+class BuildPoseidon(build_ext):
+    already_built = False
+
+    def build_extension(self, ext):
+        if self.already_built:
+            return
+
+        subprocess.run("chmod a+x ./build.sh && ./build.sh", shell=True, check=True)
+        self.already_built = True
+
+
+if __name__ == "__main__":
+    with open("README.md", encoding="utf-8") as f:
+        long_description = f.read()
+
+    setuptools.setup(
+        name="poseidon_py",
+        version="0.1.1",
+        description="Python implementation of Poseidon hash",
+        long_description=long_description,
+        author="drknzz",
+        author_email="kamil.jankowski.x@gmail.com",
+        url="https://github.com/drknzz/poseidon-py.git",
+        extras_require={"build": ["make"]},
+        ext_modules=[PoseidonExtension()],
+        cmdclass={
+            "build_py": BuildPy,
+            "build_ext": BuildPoseidon,
+        },
+        python_requires=">=3.9",
+        packages=["poseidon_py"],
+        package_data={"poseidon_py": ["../lib_pos.*"]},
+    )
